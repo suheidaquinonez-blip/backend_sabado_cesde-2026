8c0e0756fc5d62817b1b68731b184fded3e1a344
kevin florec <darkonoises@gmail.com>
2026-02-07 14:11:18 -0500

    repaso java

---
+++ src/application/Main.java
@@ -0,0 +1,34 @@
+package application;
+
+public class Main {
+
+    public static void main (String []args) {
+        System.out.println("hello,word!");
+        String name;
+        int age;
+        boolean male;
+        int BIRTH_YEAR;
+
+        name = "Genesis";
+        age = 38;
+        male = true;
+
+        BIRTH_YEAR = 2003
+
+        System.out.println("my name is" + name);
+        System.out.println("i am" + age + "year old");
+        System.out.println("am i male?" + male );
+
+        final int CURRENTYEAR = 2026;
+         //CURRENTEYEAR
+
+        int birthYear =2003;
+        int calculatedAge = CURRENTYEAR - birthYear;
+
+        System.out.println("calculated age: " + calculatedAge);
+
+        for (int i = 1; i <= 10; i++);
+        System.out.println("interetion:" + i);
+
+    }
+}
